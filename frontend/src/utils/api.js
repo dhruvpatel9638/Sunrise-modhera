d9e65c938981e70d63135f36a5f715d8eff779b4
dhruvpatel9638 <dhruvp9638@gmail.com>
2026-09-18 10:00:37 +0530

    Update API_BASE_URL to live Render backend https://sunrise-modhera-1.onrender.com/api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://sunrise-modhera.onrender.com/api';
+const API_BASE_URL = 'https://sunrise-modhera-1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
